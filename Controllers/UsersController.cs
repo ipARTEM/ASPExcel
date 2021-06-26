35c68e363b10183dfc63074af4d4ea406bddec27
Artem <a.khimin@yandex.ru>
2021-06-26 23:47:38 +0300

    Export/Import Excel

---
+++ Controllers/UsersController.cs
@@ -0,0 +1,44 @@
+﻿using ASPExcel.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+
+namespace ASPExcel.Controllers
+{
+    public class UsersController :Controller
+    {
+        public IActionResult Index()
+        {
+            var users = GetUsersList();
+
+            return View(users);
+        }
+
+        private List<User> GetUsersList()
+        {
+            var users = new List<User>()
+            {
+                    new User{
+                Email="a.khimin@yandex.ru",
+                    Name="Artem",
+                    Phone="8888888",
+            },
+                                        new User{
+                Email="a.khimin@yandex.ru",
+                    Name="Andrey",
+                    Phone="9999999",
+            },
+                                                            new User{
+                Email="a.khimin@yandex.ru",
+                    Name="Petya",
+                    Phone="7777777",
+            },
+            };
+
+            return users;
+
+
+        }
+        
+
+    }
+}
