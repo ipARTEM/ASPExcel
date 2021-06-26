38996493c643c96080ca20e2c57806569385e3a1
Artem <a.khimin@yandex.ru>
2021-06-27 00:30:36 +0300

    ExportToExcel

--- Controllers/UsersController.cs
+++ Controllers/UsersController.cs
@@ -1,6 +1,8 @@
 ﻿using ASPExcel.Models;
 using Microsoft.AspNetCore.Mvc;
+using OfficeOpenXml;
 using System.Collections.Generic;
+using System.IO;
 
 namespace ASPExcel.Controllers
 {
@@ -11,6 +13,56 @@ namespace ASPExcel.Controllers
             var users = GetUsersList();
 
             return View(users);
+        }
+
+        public IActionResult ExportToExcel()
+        {
+            var users = GetUsersList();
+
+            var stream = new MemoryStream();
+
+            using (var xlPackage=new ExcelPackage(stream))
+            {
+                var worksheet = xlPackage.Workbook.Worksheets.Add("Users");
+
+                var customStyle = xlPackage.Workbook.Styles.CreateNamedStyle("CustomStyle");
+                customStyle.Style.Font.UnderLine = true;
+                //customStyle.Style.Font.Color.SetColor(Color.Red);
+
+                var starRow = 5;
+                var row = starRow;
+
+                worksheet.Cells["A1"].Value = "Test Export";
+
+                using(var r=worksheet.Cells["A1:C1"])
+                {
+                    r.Merge = true;
+                    //r.Style.Fill.BackgroundColor.SetColor(  );
+                    
+                }
+                worksheet.Cells["A4"].Value = "Name";
+                worksheet.Cells["B4"].Value = "Email";
+                worksheet.Cells["C4"].Value = "Phone";
+
+                row = 5;
+                foreach (var user in users)
+                {
+                    worksheet.Cells[row, 1].Value = user.Name;
+                    worksheet.Cells[row, 2].Value = user.Email;
+                    worksheet.Cells[row, 3].Value = user.Phone;
+
+                    row++;
+                }
+
+                xlPackage.Workbook.Properties.Title = "User lost";
+                xlPackage.Workbook.Properties.Author = "Mohamad";
+
+                xlPackage.Save();
+            }
+
+            stream.Position = 0;
+            return File(stream, "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet", "users.xlsx");
+
         }
 
         private List<User> GetUsersList()
