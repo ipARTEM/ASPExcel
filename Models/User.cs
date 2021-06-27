4888876c721f12efea7df43e5b886301f735f0e4
Artem <a.khimin@yandex.ru>
2021-06-27 22:19:35 +0300

    Корректировка Models

--- Models/User.cs
+++ Models/User.cs
@@ -12,5 +12,8 @@ namespace ASPExcel.Models
         public string Email { get; set; }
 
         public string Phone { get; set; }
+        public string Phone2 { get; set; }
+        public string Phone3 { get; set; }
+
     }
 }
