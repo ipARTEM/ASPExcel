353a381cf991d3087c925d8f732f15d7f8481a32
Artem <a.khimin@yandex.ru>
2021-06-27 14:45:35 +0300

    Import from Excel

--- Controllers/UsersController.cs
+++ Controllers/UsersController.cs
@@ -1,8 +1,11 @@
 ﻿using ASPExcel.Models;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using OfficeOpenXml;
+using System;
 using System.Collections.Generic;
 using System.IO;
+using System.Linq;
 
 namespace ASPExcel.Controllers
 {
@@ -64,6 +67,70 @@ namespace ASPExcel.Controllers
             return File(stream, "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet", "users.xlsx");
 
         }
+
+        [HttpGet]
+        public IActionResult BatchUserUpload()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult BatchUserUpload(IFormFile batchUsers)
+        {
+            if (ModelState.IsValid)
+            {
+                if (batchUsers?.Length > 0)
+                {
+                    // convert to a stream
+                    var stream = batchUsers.OpenReadStream();
+
+                    List<User> users = new List<User>();
+
+                    try
+                    {
+                        using (var package = new ExcelPackage(stream))
+                        {
+                            var worksheet = package.Workbook.Worksheets.First();
+                            var rowCount = worksheet.Dimension.Rows;
+
+                            for (var row = 2; row <= rowCount; row++)
+                            {
+                                try
+                                {
+                                    var name = worksheet.Cells[row, 1].Value?.ToString();
+                                    var email = worksheet.Cells[row, 2].Value?.ToString();
+                                    var phone = worksheet.Cells[row, 3].Value?.ToString();
+
+                                    var user = new User()
+                                    {
+                                        Email = email,
+                                        Name = name,
+                                        Phone = phone
+                                    };
+
+                                    users.Add(user);
+                                }
+                                catch (Exception ex)
+                                {
+                                    Console.WriteLine(ex.Message);
+                                }
+                            }
+                        }
+
+                        return View("Index", users);
+                    }
+                    catch (Exception ex)
+                    {
+                        Console.WriteLine(ex.Message);
+                    }
+                }
+            }
+
+            return View();
+        }
+
+
 
         private List<User> GetUsersList()
         {
